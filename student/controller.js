25ad2f9dba2d7d1e4542c1b8f3ef1c289228b46f
mo-cherif <mocherifh0@gmail.com>
2023-06-06 18:01:11 +0100

    uploading...

--- student/controller.js
+++ student/controller.js
@@ -1,3 +1,23 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+})
+
 const uploadImage = (req,res) => {
-    
+    const upload = multer({ storage: storage })
+    upload(req, res, (err) => {
+        if(err) {
+            return res.status(500).send({message: "Failed to upload the image."});
+        }
+
+        return res.status(200).send({message: "Image uploaded successfully."});
+    })
 }
+
+export default uploadImage;
--- student/route.js
+++ student/route.js
@@ -1,11 +1,16 @@
 import {Router}  from 'express';
+import multer from "multer";
+import uploadImage from './controller';
+
+// Multer configuration
+const upload = multer();
 
 const studentRouter = Router();
 
 /**
  * Student
  */
-studentRouter.post("/student", () => {})
+studentRouter.post("/student", upload.single('image'), uploadImage)
 studentRouter.get("/studentImage/:id", () => {})
 studentRouter.put("/studentImage/:id", () => {})
 studentRouter.delete("/studentImage/:id", () => {})
